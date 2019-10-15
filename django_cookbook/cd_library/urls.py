6e962c435ef5f962f799303c5251d7e3acd937e7
volderpayoda <volderpayoda@gmail.com>
2019-10-15 11:29:36 -0300

    Agregando create y delete

--- django_cookbook/cd_library/urls.py
+++ django_cookbook/cd_library/urls.py
@@ -4,7 +4,11 @@ from . import views
 app_name = 'cd_library'
 urlpatterns = [
     # ex: /cd_library/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # ex: /cd_library/5
-    path('<int:pk>', views.detail, name='detail')
+    path('<int:pk>', views.DetailView.as_view(), name='detail'),
+    # ex: /cd_library/create
+    path('create/', views.CreateView.as_view(), name="create"),
+    # ex: /cd_library/5/delete
+    path('<int:pk>/delete', views.DeleteView.as_view(), name="delete")
 ]
--- django_cookbook/cd_library/views.py
+++ django_cookbook/cd_library/views.py
@@ -1,19 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import CD
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, DeleteView
+from django.urls import reverse, reverse_lazy
 
 # Create your views here.
 
-def index(request):
-    query_set = CD.objects.all()
-    context = {
-        'cds': query_set
-    }
-    return render(request, 'cd_library/index.html', context)
+class IndexView(ListView):
+    model = CD
+    template_name = 'cd_library/index.html'
+    context_object_name = 'cds'
 
-def detail(request, pk):
-    query_set = CD.objects.get(pk=pk)
-    context = {
-        'cd': query_set
-    }
-    return render(request, 'cd_library/detail.html', context)
+class DetailView(DetailView):
+    model = CD
+    template_name = 'cd_library/detail.html'
+    context_object_name = 'cd'
+
+class CreateView(CreateView):
+    model = CD
+    fields = '__all__'
+    template_name = 'cd_library/create.html'
+    success_url = reverse_lazy('cd_library:index')
+    
+class DeleteView(DeleteView):
+    model = CD
+    template_name = 'cd_library/delete.html'
+    context_object_name = 'cd'
+    success_url = reverse_lazy('cd_library:index')
